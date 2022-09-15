d6a7955f650196cdc621880abedf399054bea9f9
PrathikshaKini <kiniprathiksha18@gmail.com>
2022-09-15 23:21:26 +0530

    Added views and django template setup

--- socials/base/views.py
+++ socials/base/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    context={}
+    return render(request,"base/home.html",context)
+    
+def login(request):
+    context={}
+    return render(request,"base/login.html",context)
+
+def signup(request):
+    context={}
+    return render(request,"base/signup.html",context)
